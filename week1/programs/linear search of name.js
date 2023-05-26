c3eb37b6d1f695a92d76bfc179e854229d602910
Jijin vj <98830107+Jijinvj21@users.noreply.github.com>
2023-05-26 11:03:53 +0530

    Delete week1 directory

--- week1/programs/linear search of name.js
+++
@@ -1,9 +0,0 @@
-const names = ['Alice', 'Bob', 'Charlie', 'David', 'Eve']
-const searchName = 'Charlie'
-
-for (let i = 0; i < names.length; i++) {
-  if (names[i] === searchName) {
-    console.log(`Found ${searchName} at index ${i}`)
-    return
-  }
-}
